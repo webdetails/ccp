9f4ed238e9e2953bd33be3c6887a7ee5ee07a1cd
Pedro Alves <pedro.alves@webdetails.pt>
2015-08-13 18:31:15 +0100

    - Added forum rss feed

--- static/js/main.js
+++ static/js/main.js
@@ -44,50 +44,71 @@ CCP.liveChat = function(){
 
 }
 
+/* Forum Feed - Se more info in https://github.com/sdepold/jquery-rss */
+CCP.getForumFeed = function(divId){
+
+	var _url = "http://forums.pentaho.com/external.php?type=rss2";
+
+	$(divId).rss(_url,{
+			limit: 5,
+			layoutTemplate: "<div class='feed-container'>{entries}</div>",
+			entryTemplate: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>',
+			effect: "show",
+			dateFormat: 'ddd, HH:mm',
+			error: function() { throw new Error("jQuery RSS: url don't link to RSS-Feed") }
+		});
+}
+
+
 
 $(document).ready(function() {
-	$('#scrollDownBtn, #scrollDownBtnMobile').click(function(event) {
-		$("body, html").animate({
-		    scrollTop: $("#ccp-02").position().top
-		});
-	});
-	addShare();
+		$('#scrollDownBtn, #scrollDownBtnMobile').click(function(event) {
+				$("body, html").animate({
+						scrollTop: $("#ccp-02").position().top
+					});
+			});
+		addShare();
 
 
-	$('#tech-info a').click(function (e) {
-  	e.preventDefault()
-  	$(this).tab('show')
-	})
+		$('#tech-info a').click(function (e) {
+				e.preventDefault()
+				$(this).tab('show')
+			})
 
-  var isMobile = (/iphone|ipad|ipod|android|ie|blackberry|fennec/).test(navigator.userAgent.toLowerCase());
-	if (isMobile == true){
-		$('body').addClass("mobile");
+		var isMobile = (/iphone|ipad|ipod|android|ie|blackberry|fennec/).test(navigator.userAgent.toLowerCase());
+		if (isMobile == true){
+			$('body').addClass("mobile");
 
-	} else {
-		$('body').addClass("desktop");
-	}
-
-
-	$(window).scroll(function() {
-	    var y_scroll_pos = window.pageYOffset;
-	    var scroll_pos_test = $('#ccp-03').offset().top - 20;
-
-	    if(y_scroll_pos > scroll_pos_test) {
-				// activate the all mighty sticky footer
-				$('body').addClass("sticky-footer");
-				if ($('body').hasClass("desktop")) {
-					$('header').css("display","block");
-				} else {
-					$('header').css("display","none");
-				}
-	    }
-		else
-		{
-			// de-activate the all mighty sticky footer
-			$('body').removeClass("sticky-footer");
-			$('header').css("display","block");
-
+		} else {
+			$('body').addClass("desktop");
 		}
-	});
 
-});
+
+		$(window).scroll(function() {
+				var y_scroll_pos = window.pageYOffset;
+				var scroll_pos_test = $('#ccp-03').offset().top - 20;
+
+				if(y_scroll_pos > scroll_pos_test) {
+					// activate the all mighty sticky footer
+					$('body').addClass("sticky-footer");
+					if ($('body').hasClass("desktop")) {
+						$('header').css("display","block");
+					} else {
+						$('header').css("display","none");
+					}
+				}
+				else
+				{
+					// de-activate the all mighty sticky footer
+					$('body').removeClass("sticky-footer");
+					$('header').css("display","block");
+
+				}
+			});
+
+		// Set the feeder
+
+		CCP.getForumFeed("#forumQuestions");
+
+
+	});
